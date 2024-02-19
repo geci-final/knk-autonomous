f79e82f117c776f8128bfda7d7bbe0c4107a0b9c
v4zha <arvin_j4848@protonmail.com>
2024-02-19 19:15:26 +0530

    remove knk_transformer

--- knk_model/setup.py
+++ knk_model/setup.py
@@ -1,5 +1,5 @@
 from setuptools import setup
-setup(name='knk_transformers',
+setup(name='knk_model',
       version='0.0.1',
-      packages=['knk_transformers'],
+      packages=['knk_model'],
       install_requires=['torch'])
