f1df671c270460d26c1fb66354ad82d0e582bcec
v4zha <arvin_j4848@protonmail.com>
2024-02-24 23:11:15 +0530

    Update .gitignore and modify detector.py

--- knk_vision/knk-detector/detector.py
+++ knk_vision/knk-detector/detector.py
@@ -1,8 +1,9 @@
+import time
 import torch
 import cv2
 import numpy as np
 from ultralytics import RTDETR
-from typing import List
+from typing import List, Tuple, Optional
 
 
 class KnkVision:
@@ -92,30 +93,67 @@ class KnkVision:
                           'teddy bear',
                           'hair drier',
                           'toothbrush']
-        self.midas = torch.hub.load("intel-isl/MiDaS", "DPT_Hybrid")
-        self.midas.to(self.device)
-        self.midas.eval()
-        m_transform = torch.hub.load(
-            "intel-isl/MiDaS", "transforms")
-        self.midas_transform = m_transform.dpt_transform
+        # torch.hub.help("intel-isl/MiDaS", "DPT_BEiT_L_384", force_reload=True)
+        # fix for timm>=0.9.8
+        # issue : https://github.com/isl-org/ZoeDepth/issues/82
+        self.zoe = torch.hub.load(
+            "isl-org/ZoeDepth", "ZoeD_K", pretrained=False)
+        pretrained_dict = torch.hub.load_state_dict_from_url(
+            'https://github.com/isl-org/ZoeDepth/releases/download/v1.0/ZoeD_M12_K.pt', map_location='cpu')
+        self.zoe.load_state_dict(pretrained_dict['model'], strict=False)
+        for b in self.zoe.core.core.pretrained.model.blocks:
+            b.drop_path = torch.nn.Identity()
+        self.zoe.to(self.device)
+        self.zoe.eval()
 
     def depth(self, img: np.ndarray) -> np.ndarray:
-        inp_img = self.midas_transform(img).to(self.device)
-        with torch.no_grad():
-            prediction = self.midas(inp_img)
-            prediction = torch.nn.functional.interpolate(
-                prediction.unsqueeze(1),
-                size=img.shape[:2],
-                mode="bicubic",
-                align_corners=False,
-            ).squeeze()
-        return prediction.cpu().numpy()
+        start_time = time.time()
+        img /= 255.0
+        img = torch.from_numpy(img).permute(
+            2, 0, 1).unsqueeze(0).to(self.device)
+        depth = self.zoe.infer(img)
+        depth = depth.squeeze().detach().cpu().numpy()
+        end_time = time.time()
+        print(f'Inference time for ZoeDepth : {end_time-start_time:.2f}s')
+        return depth
 
     def detect(self, img: np.ndarray):
         return self.redetr(img)
 
-    def calc_dist(self, depth: np.ndarray, bbox: np.ndarray) -> float:
-        return np.mean(depth[bbox[1]:bbox[3], bbox[0]:bbox[2]])
+    def calc_dist(self, depth: np.ndarray, xyxy: List[int]) -> float:
+        x1, y1, x2, y2 = xyxy
+        dist = depth[y1:y2, x1:x2].mean()
+        return dist
+
+    def detect_and_calc_dist(self, frame: np.ndarray) -> Optional[List]:
+        # return type of dict
+        # {
+        # class_name,
+        # distance,
+        # bounding_box
+        # dimensions
+        # }
+        results = self.detect(frame)[0]
+        if results:
+            res_objects = []
+            img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            img = img.astype(np.float32)
+            depth = self.depth(img)
+            boxes = results.boxes.cpu().numpy()
+            for box in boxes:
+                if box.conf < 0.5:
+                    continue
+                xyxy = box.xyxy[0].astype(int)
+                dist = self.calc_dist(depth, xyxy)
+                cls_name = self.cls_names[int(box.cls[0])]
+                res_objects.append({
+                    'class_name': cls_name,
+                    'distance': dist,
+                    'bounding_box': xyxy,
+                })
+            return res_objects
+        else:
+            return None
 
 
 def main():
@@ -126,25 +164,18 @@ def main():
         while True:
             ret, frame = cap.read()
             if ret:
-                img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-                # depth = vision.depth(img)
-                results = vision.detect(frame)[0]
-                if results:
-                    boxes = results.boxes.cpu().numpy()
-                    for box in boxes:
-                        if box.conf < 0.5:
-                            continue
-                        cls = int(box.cls[0])
-                        cls_name = vision.cls_names[cls]
-                        xyxy=box.xyxy[0].astype(int)
+                res_objects = vision.detect_and_calc_dist(frame)
+                if res_objects:
+                    for obj in res_objects:
+                        cls_name = obj['class_name']
+                        xyxy = obj['bounding_box']
                         x1, y1, x2, y2 = xyxy
                         cv2.rectangle(frame, (x1, y1),
                                       (x2, y2), (0, 255, 0), 2)
-                        # dist = vision.calc_dist(depth, box)
-                        dist = 0
+                        dist = obj['distance']
                         cv2.putText(frame, f'[{cls_name}] || {dist:.2f}m', (int(x1+3), int(
                             y1+3)), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
-                cv2.imshow('frame_depth', frame)
+                cv2.imshow('Knk-Vision', frame)
                 if cv2.waitKey(1) & 0xFF == ord('q'):
                     break
             else:
